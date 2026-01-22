27e4a5d83ce12b551af992b01f29fdd8fa847a76
Ryan Marx <ryan.marx@pulsarinformatics.com>
2026-01-21 22:17:17 -0600

    update tests

--- src/config/config.test.js
+++ src/config/config.test.js
@@ -1,14 +1,10 @@
-const { test, describe } = require("node:test");
-const assert = require("assert/strict");
-const fs = require("node:fs/promises");
-const path = require("node:path");
+import { test, describe, expect } from "vitest";
+import fs from "node:fs/promises";
+import { Validator } from "jsonschema";
 
-const Validator = require("jsonschema").Validator;
 const v = new Validator();
 
-describe("Validate config files against schemas", validateSchemas);
-
-async function validateSchemas() {
+describe("Validate config files against schemas", async () => {
 	const files = await fs.readdir("./src/config");
 
 	// Look for our schema files and collect the filenames/slugs
@@ -19,26 +15,16 @@ async function validateSchemas() {
 		return acc;
 	}, new Set());
 
-	// For each schema file, test its corresponding json. Config files without schemas are assumed to be immaculate
-	for (let t of tests) {
-		test(`JSON validation:  ${t}`, async () => {
+	// For each schema file, test its corresponding json
+	for (const t of tests) {
+		test(`JSON validation: ${t}`, async () => {
 			const [data, schema] = await Promise.all([
-				fs
-					.readFile(`./src/config/${t}.json`, "utf8")
-					.then(JSON.parse)
-					.catch(e => {
-						assert(false, e);
-					}),
-				fs
-					.readFile(`./src/config/${t}.schema.json`, "utf8")
-					.then(JSON.parse)
-					.catch(e => {
-						assert(false, e);
-					}),
+				fs.readFile(`./src/config/${t}.json`, "utf8").then(JSON.parse),
+				fs.readFile(`./src/config/${t}.schema.json`, "utf8").then(JSON.parse),
 			]);
-			const validation = v.validate(data, schema);
 
-			assert.equal(validation.errors.length, 0, `${t}.json failed validation`);
+			const validation = v.validate(data, schema);
+			expect(validation.errors).toHaveLength(0);
 		});
 	}
-}
+});
